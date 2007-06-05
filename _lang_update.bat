65deaec0281329713294a257612600e7b878fa51
valex <valex@localhost>
2007-06-05 21:16:09 +0000

    lang update: do fuzzy matching

--- _lang_update.bat
+++ _lang_update.bat
@@ -1,4 +1,4 @@
 @echo off
 intl\bin\xgettext -f ac3filter_intl.files -d ac3filter -o ac3filter.pot -p lang -k_ -kN_ --add-comments=TRANSLATORS:
-for %%f in (lang\*.po) do intl\bin\msgmerge --update --no-fuzzy-matching %%f lang\ac3filter.pot
+for %%f in (lang\*.po) do intl\bin\msgmerge --update %%f lang\ac3filter.pot
 for %%f in (lang\*.po) do intl\bin\msgfmt lang\%%~nf.po -o lang\%%~nf.mo
