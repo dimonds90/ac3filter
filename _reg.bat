d17fa2a07abda11399bc2e8f124de3ad4dc87d7c
valex <valex@localhost>
2007-08-08 15:54:14 +0000

    Copy msvcrtd to the debug folder

--- _reg.bat
+++ _reg.bat
@@ -2,5 +2,7 @@
 echo ------------------------
 echo Register %1 version
 copy /b intl\bin\iconv.dll %1\
+if "%1" == "debug" copy /b %windir%\system32\msvcrtd.dll %1\
+if "%1" == "DEBUG" copy /b %windir%\system32\msvcrtd.dll %1\
 regsvr32 /s /u %1\ac3filter.ax
 regsvr32 /s %1\ac3filter.ax
