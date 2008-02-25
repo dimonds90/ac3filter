ed97bcf9a49580016edb497dccbc185767330501
valex <valex@localhost>
2008-02-25 20:00:02 +0000

    i18n: no line numbers at POT file, disable fuzzy matching on update

--- _lang_update.bat
+++ _lang_update.bat
@@ -10,11 +10,11 @@ goto update_translations
 
 :build_new_pot
 echo Building translation template
-intl\bin\xgettext -f ac3filter_intl.files --language=c++ -d ac3filter -o ac3filter.pot -p lang -k_ -kN_ --add-comments=TRANSLATORS:
+intl\bin\xgettext -f ac3filter_intl.files --no-location --language=c++ -d ac3filter -o ac3filter.pot -p lang -k_ -kN_ --add-comments=TRANSLATORS:
 
 :update_translations
 echo Updating translation files
-for %%f in (lang\*.po) do intl\bin\msgmerge --update %%f lang\ac3filter.pot
+for %%f in (lang\*.po) do intl\bin\msgmerge --update --no-fuzzy-matching %%f lang\ac3filter.pot
 for %%f in (lang\*.po) do intl\bin\msgfmt lang\%%~nf.po -o lang\%%~nf.mo
 
 del lang\ac3filter_new.pot >nul 2>nul
