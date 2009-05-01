d727144b1462f24f58c641e7bd740e996751c481
valex <valex@localhost>
2009-05-01 04:37:58 +0000

    * ControlSpectrum: automatically adjust cache size

--- filter/dlg/control_spectrum.cpp
+++ filter/dlg/control_spectrum.cpp
@@ -30,7 +30,7 @@ static const double min_db = -100;
 static const unsigned lin_window_length = 512;
 static const unsigned log_window_length = 1024;
 static const int sample_rate = 48000;
-static const vtime_t cache_size = 4.0;
+static const vtime_t max_lag_time = 5.0;
 
 ControlSpectrum::ControlSpectrum(HWND _dlg, IAC3Filter *_filter, IAudioProcessor *_proc):
 Controller(_dlg, ::controls), filter(_filter), proc(_proc), length(0)
@@ -71,8 +71,6 @@ ControlSpectrum::init_spectrum(unsigned new_length)
     return;
   }
 
-  proc->set_output_cache_size(cache_size);
-
   double alpha = kaiser_alpha(-min_db);
   int odd_length = length / 2 - 1;
   for (int i = 0; i <= 2 * odd_length; i++)
@@ -103,13 +101,24 @@ void ControlSpectrum::update_dynamic()
   // Show nothing when uninitialized
   if (!is_ok()) return;
 
+  // Automatically adjust cache size
+  vtime_t playback_time, cache_time, cache_size, lag_time;
+  proc->get_output_cache_size(&cache_size);
+  proc->get_output_cache_time(&cache_time);
+  filter->get_playback_time(&playback_time);
+  lag_time = cache_time - playback_time + vtime_t(length) / spk.sample_rate;
+  if (lag_time > max_lag_time)
+    lag_time = max_lag_time;
+
+  if (lag_time > cache_size)
+    proc->set_output_cache_size(lag_time + 1.0);
+
   // Get data
   filter->get_playback_time(&time);
   time -= vtime_t(length / 2) / spk.sample_rate;
   proc->get_output_cache(time, buf, length);
 
   // Sum channels
-
   for (int ch = 1; ch < spk.nch(); ch++)
     for (i = 0; i < length; i++)
       data[i] += buf[ch][i];
