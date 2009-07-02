170947cb35136c297b18e39d27241da5a515537c
valex <valex@localhost>
2009-07-02 12:46:34 +0000

    build_dist makes timestamped builds by default (instaed of just 'internal' version)

--- build_dist.cmd
+++ build_dist.cmd
@@ -2,8 +2,10 @@
 
 set ver_text=%1
 set ver_file=%2
-if "%ver_text%"=="" set ver_text=internal
-if "%ver_file%"=="" set ver_file=internal
+set timestamp=%date:~8,2%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%
+
+if "%ver_text%"=="" set ver_text=test%timestamp%
+if "%ver_file%"=="" set ver_file=test%timestamp%
 
 rem -------------------------------------------------------
 rem Clean all and make the source distribution
