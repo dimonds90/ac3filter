2c2bb12911052143e80768dba03b64a5662333b2
valex <valex@localhost>
2012-04-28 07:03:59 +0400

    Close error report dlg when sending a report. Send user message.

--- filter/dlg/error_report.h
+++ filter/dlg/error_report.h
@@ -7,14 +7,14 @@
 class ErrorReportDlg : public DialogBase
 {
 protected:
-  TextEdit edt_feedback;
-  TextEdit edt_desc;
-
   void update();
   virtual void on_create();
   virtual BOOL on_message(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 public:
+  TextEdit edt_feedback;
+  TextEdit edt_desc;
+
   ErrorReportDlg(): DialogBase()
   {}
 };
